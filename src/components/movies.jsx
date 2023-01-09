780e5c0bcf46f5fcdaaa208bd8ab7f898f490797
Patrick Espake <patrick@envixo.com>
2023-01-09 19:03:30 +0000

    improvements to show when we dont have movies in the database

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -11,47 +11,54 @@ class Movies extends Component {
   };
 
   render() {
+    const { length: count } = this.state.movies;
+
+    if (count === 0) return <p>There are no movies in the database.</p>;
+
     return (
-      <div className="py-5 bg-light">
-        <div className="container">
-          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
-            {this.state.movies.map((movie) => (
-              <div className="col" key={movie.id}>
-                <div className="card shadow-sm h-100">
-                  <img
-                    src={movie.image.replace("400x", "400x100")}
-                    className="card-img-top"
-                    alt="Movie"
-                  />
-                  <div className="card-header">{movie.name}</div>
-                  <div className="card-body">
-                    <div className="d-flex justify-content-between align-items-center">
-                      <p className="card-text">$ {movie.market_price}</p>
-                      <button
-                        type="button"
-                        className="btn btn-sm btn-outline-secondary"
-                        onClick={() => this.handleView(movie)}
-                      >
-                        View
-                      </button>
+      <>
+        <div className="py-5 bg-light">
+          <div className="container">
+            <p className="g-4">Showing {count} movies in the database.</p>
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
+              {this.state.movies.map((movie) => (
+                <div className="col" key={movie.id}>
+                  <div className="card shadow-sm h-100">
+                    <img
+                      src={movie.image.replace("400x", "400x100")}
+                      className="card-img-top"
+                      alt="Movie"
+                    />
+                    <div className="card-header">{movie.name}</div>
+                    <div className="card-body">
+                      <div className="d-flex justify-content-between align-items-center">
+                        <p className="card-text">$ {movie.market_price}</p>
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                          onClick={() => this.handleView(movie)}
+                        >
+                          View
+                        </button>
+                      </div>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                      {movie.genres.map((genre) => (
+                        <li className="list-group-item" key={genre.id}>
+                          {genre.name}
+                        </li>
+                      ))}
+                    </ul>
+                    <div className="card-footer text-muted">
+                      Owner: {movie.owner.name}
                     </div>
                   </div>
-                  <ul className="list-group list-group-flush">
-                    {movie.genres.map((genre) => (
-                      <li className="list-group-item" key={genre.id}>
-                        {genre.name}
-                      </li>
-                    ))}
-                  </ul>
-                  <div className="card-footer text-muted">
-                    Owner: {movie.owner.name}
-                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
